0c32b0b4db7dc45a32c0d7a000b4c68aec45cbf9
Volodymyr Hinchuk <teikonami@gmail.com>
2023-08-19 15:26:36 +0900

    Update contact-form.php

--- contact-form.php
+++ contact-form.php
@@ -2,7 +2,7 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "teikonami@gmail.com";
+    $email_to = "example@mail.com";
     $email_subject = "New form submissions";
 
     function problem($error)
